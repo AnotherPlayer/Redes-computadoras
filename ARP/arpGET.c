7b48e9d36f751091124cb59279334a4be386a51e
Another_One <hd.tetuan5002@gmail.com>
2026-01-07 14:08:01 -0600

    Propuesta_1_ARP

--- ARP/arpGET.c
+++ ARP/arpGET.c
@@ -1,4 +1,4 @@
-//Obtener
+//Obtener (Reply)
 
 #include <stdio.h>
 #include <net/if.h>
@@ -19,7 +19,54 @@ unsigned char IPdestino[4];
 unsigned char etherARP[2]={0x08,0x06};
 unsigned char codARPresp[2]={0x00,0x02};
 
-int filtroARP(unsigned char *paq,int len){
+//Check --> Parece estar bien
+int obtenerDatos( int ds ){
+
+    struct ifreq red;
+    char nombre[8];
+    int i, index;
+
+    // Solicita el nombre de la interfaz (ej. eth0, wlan0)
+    printf("\nNombre de la interfaz de red: ");
+    gets(nombre);
+    strcpy(red.ifr_name, nombre);
+
+    // 1. Obtención del índice de la interfaz
+    if(ioctl(ds, SIOCGIFINDEX, &red) == -1) {
+        perror("\nError al obtener el indice");
+        exit(0);
+    }
+    index = red.ifr_ifindex;
+
+    // 2. Obtención de la dirección MAC origen
+    if(ioctl(ds, SIOCGIFHWADDR, &red) == -1) {
+        perror("\nError al obtener la MAC");
+        exit(0);
+    }
+    memcpy(MACorigen, red.ifr_hwaddr.sa_data, 6);
+
+    // 3. Obtención de la dirección IP origen
+    if(ioctl(ds, SIOCGIFADDR, &red) == -1) {
+        perror("\nError al obtener la IP");
+        exit(0);
+    }
+    // Se copian 4 bytes a partir del byte 2 por la estructura sockaddr_in
+    memcpy(IPorigen, red.ifr_addr.sa_data + 2, 4);
+
+    // Impresión de verificación de los datos locales
+    printf("El indice es: %d\n", index);
+    printf("La MAC origen es: ");
+    for(i = 0; i < 6; i++) printf("%.2X%c", MACorigen[i], i < 5 ? ':' : ' ');
+    printf("\nLa IP origen es: ");
+    for(i = 0; i < 4; i++) printf("%d%c", IPorigen[i], i < 3 ? '.' : ' ');
+    printf("\n");
+
+    return index;
+
+}
+
+//Done --> Es del libro
+int filtroARP( unsigned char *paq,int len ){
 
     if(!memcmp(paq,MACorigen,6)){
         if(!memcmp(paq+12,etherARP,2))
@@ -33,13 +80,14 @@ int filtroARP(unsigned char *paq,int len){
 
 }
 
-void recibeARPresp(int ds,unsigned char *trama){
+//Done --> Es del libro
+void recibeARPresp( int ds,unsigned char *trama ){
 
     int tam,bandera=0;
 
     while(1){
 
-        tam=recvfrom(ds,trama,1514,0,NULL,0);
+        tam = recvfrom(ds,trama,1514,0,NULL,0);
 
         if(tam==-1)
             perror("\nError al recibir");
@@ -56,10 +104,27 @@ void recibeARPresp(int ds,unsigned char *trama){
 
 }
 
+//Check --> Parece estar bien
+void imprimeTrama( unsigned char *trama, int tam ) {
+    
+    int i;
+
+    for(i = 0; i < tam; i++) {
+
+        if(i % 16 == 0) printf("\n");
+            printf("%.2x ", trama[i]);
+
+    }
+
+    printf("\n");
+
+}
+
+//Done --> Es del libro
 int main(){
 
     int packet_socket,indice;
-    packet_socket=socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+    packet_socket = socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
 
     if(packet_socket==-1){
 
@@ -71,9 +136,12 @@ int main(){
     else{
 
         perror("Exito al abrir el socket");
+
         indice = obtenDatos(packet_socket);
         recibeARPresp(packet_socket,tramaARPresp);
-        printf("\n**********La trama que se recibe de respuesta ARPes*************\n");
+
+        printf("\n**********La trama que se recibe de respuesta ARP's*************\n");
+
         imprimeTrama(tramaARPresp,60);
 
     }
--- ARP/arpSET.c
+++ ARP/arpSET.c
@@ -1,4 +1,4 @@
-//Enviar
+//Enviar (Request)
 
 #include <stdio.h>
 #include <net/if.h>
@@ -15,21 +15,85 @@ unsigned char tramaARPsol[60]=
 {0xff,0xff,0xff,0xff,0xff,0xff,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x06,0x00,0x01,
 0x08,0x00,0x06,0x04,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'G','S','Q'};
+0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,'G','T','H'};
 
-void obtenerDatos(){
+unsigned char MACorigen[6];
+unsigned char IPorigen[4];
+unsigned char IPdestino[4];
+unsigned char etherARP[2]={0x08,0x06};
+unsigned char codARPresp[2]={0x00,0x02};
 
+//Check --> Parece estar bien
+void obtenerDatos( int ds ){
 
+    struct ifreq red;
+    char nombre[8];
+    int i, index;
+
+    printf("\nNombre de la interfaz de red: ");
+    gets(nombre); // El libro utiliza gets para capturar el nombre [1]
+    strcpy(red.ifr_name, nombre);
+
+    // 1. Obtención del índice de la interfaz [9]
+    if(ioctl(ds, SIOCGIFINDEX, &red) == -1) {
+        perror("\nError al obtener el indice");
+        exit(0);
+    }
+
+    index = red.ifr_ifindex;
+
+    // 2. Obtención de la dirección MAC (física) [4, 10]
+    if(ioctl(ds, SIOCGIFHWADDR, &red) == -1) {
+        perror("\nError al obtener la MAC");
+        exit(0);
+    }
+
+    memcpy(MACorigen, red.ifr_hwaddr.sa_data, 6);
+
+    // 3. Obtención de la dirección IP (lógica) [5, 11]
+    if(ioctl(ds, SIOCGIFADDR, &red) == -1) {
+        perror("\nError al obtener la IP");
+        exit(0);
+    }
+
+    // Se copia a partir del byte 2 por la estructura sockaddr_in [3, 12]
+    memcpy(IPorigen, red.ifr_addr.sa_data + 2, 4);
+
+    // Impresión de datos para verificación [13, 14]
+    printf("El indice es: %d\n", index);
+
+    printf("La MAC origen es: ");
+    for(i = 0; i < 6; i++)
+        printf("%.2X%c", MACorigen[i], i < 5 ? ':' : ' ');
+
+    printf("\nLa IP origen es: ");
+    for(i = 0; i < 4; i++)
+        printf("%d%c", IPorigen[i], i < 3 ? '.' : ' ');
+
+    printf("\n");
+
+    return index;
 
 }
 
+//Check --> Problemas con la captura de la IP destino
 void obtenerIPdestino(){
 
-
+    int ip[7];
+    printf("\nIntroduce la IP destino (ejemplo 192.168.1.1): ");
+    // El libro asume la captura de los 4 octetos [15]
+    scanf("%d.%d.%d.%d", &ip, &ip[17], &ip[18], &ip[19]);
+    
+    IPdestino = (unsigned char)ip;
+    IPdestino[17] = (unsigned char)ip[17];
+    IPdestino[18] = (unsigned char)ip[18];
+    IPdestino[19] = (unsigned char)ip[19];
 
 }
 
-void estructuraARPsol(unsigned char *trama){
+//Done --> Es del libro
+void estructuraARPsol( unsigned char *trama ){
+
     //Encabezado MAC
     memcpy(trama+6,MACorigen,6);
     //Mensaje de ARP
@@ -37,9 +101,11 @@ void estructuraARPsol(unsigned char *trama){
     memcpy(trama+28,IPorigen,4);
     memset(trama+32,0x00,6);
     memcpy(trama+38,IPdestino,4);
+
 }
 
-void enviaTrama(int ds,int index,unsigned char *paq){
+//Done --> Es del libro
+void enviaTrama( int ds,int index,unsigned char *paq ){
 
     int tam;
 
@@ -60,12 +126,21 @@ void enviaTrama(int ds,int index,unsigned char *paq){
 
 }
 
-void imprimirTrama(){
+//Check --> Parece estar bien
+void imprimirTrama( unsigned char *trama, int tam ){
 
+    int i;
 
-    
+    for(i = 0; i < tam; i++) {
+        if(i % 16 == 0) printf("\n");
+        printf("%.2x ", trama[i]);
+    }
+
+    printf("\n");
+
 }
 
+//Done --> Es del libro
 int main(){
 
     int packet_socket,indice;
